9006cc1613ee88eb47005bb9f30c29df2d8e10a0
klq26-lishuyun <klq26@163.com>
2018-10-17 21:53:24 +0800

    2018年10月17日21:52:45 更新行业估值自动化脚本，添加输出到 xlsx 的自动化脚本，简化空间压力测试的数据收集工作

---
+++ Industry/printIndustryPEPB.py
@@ -0,0 +1,38 @@
+from jqdatasdk import *
+from pandas import *
+import datetime
+import numpy
+import os
+
+auth('13810650842','123456a')
+
+start_date = '2018-9-28'
+end_date = '2018-10-16'
+
+df = DataFrame()
+
+industry_codes = ['HY001','HY002','HY003','HY004','HY005','HY006','HY007','HY008','HY010']  # HY007 + HY010 = 金融地产，HY009 基本没有公司
+
+days = get_trade_days(start_date=start_date, end_date=end_date)
+count = len(days)
+
+array = list()
+
+for code in industry_codes:
+    if os.path.exists('IndustryData\{0}.csv'.format(code)):
+        df = DataFrame.from_csv('IndustryData\{0}.csv'.format(code),sep='\t',encoding='utf-8')
+        array.append(df.tail(count))
+
+file = open('result.txt','a+')
+
+for i in range(0,count):
+    string = ''
+    for df in array:
+        if string == '':
+            string = string + df.date.values[i] + '\t'
+        string = string + str(df.value.values[i]) + '\t'
+        string = string + str(df.pe.values[i]) + '\t'
+        string = string + str(df.pb.values[i]) + '\t'
+    print(string)
+    file.write(string + '\n')
+file.close()
---
+++ Industry/updateIndustryPEPB.py
@@ -0,0 +1,71 @@
+from jqdatasdk import *
+from pandas import *
+import datetime
+import numpy
+import os
+
+auth('13810650842','123456a')
+
+df = DataFrame()
+
+industry_codes = ['HY001','HY002','HY003','HY004','HY005','HY006','HY007','HY008','HY010']  # HY007 + HY010 = 金融地产，HY009 基本没有公司
+
+for code in industry_codes:
+    if os.path.exists('IndustryData\{0}.csv'.format(code)):
+        df = DataFrame.from_csv('IndustryData\{0}.csv'.format(code),sep='\t',encoding='utf-8')
+        days = get_trade_days(start_date=df.date.values[-1].replace('/',''), end_date=datetime.date.today())
+        days = list(days)
+        days.remove(days[0])    # 去掉第一个交易日（因为来自 dataframe 说明有数据）
+        days.remove(days[-1])   # 去掉最新一个交易日（因为数据库没更新）
+
+        for day in days:
+            stocks = get_industry_stocks(code, date=day)
+            if code == 'HY007':
+                dichan = get_industry_stocks('HY010', date=day)
+                stocks.extend(dichan)
+            is_industry = True
+            count = len(stocks)
+            q = query(
+                valuation.pe_ratio,
+                valuation.pb_ratio
+            ).order_by(
+                valuation.code.asc()
+            ).filter(
+                valuation.code.in_(stocks)
+            )
+            result = get_fundamentals(q,day)
+            if len(result) == 0:
+                print('无数据')
+                continue
+
+            pes = result.pe_ratio.astype(float)
+            pbs = result.pb_ratio.astype(float)
+
+            # 指数市盈率（公式：PE = n/Σ[1/个股PE]，n为PE个数）
+            count_pe = 0
+            sum = 0
+            for i in range(0,len(pes.values)):
+                if(pes.values[i] > 0):
+                    sum += 1/pes.values[i]
+                    count_pe+=1
+            pe_index = count_pe / sum
+
+            count_pb = 0
+            sum = 0
+            for i in range(0,len(pbs.values)):
+                if(pbs.values[i] > 0):
+                    sum += 1/pbs.values[i]
+                    count_pb+=1
+            pb_index = count_pb / sum
+
+            value = 0
+
+            if is_industry:
+                value = count
+            else:
+                value = round(get_current_data()[index].last_price,2)
+
+            series = pandas.Series([day.strftime('%Y/%m/%d'),value,round(pe_index,2),round(pb_index,2)], index=['date','value','pe','pb'])
+            print(series)
+            df = df.append(series,ignore_index=True)
+    df.to_csv('IndustryData\{0}.csv'.format(code),sep='\t',columns=['date','value','pe','pb'],encoding='utf-8')
