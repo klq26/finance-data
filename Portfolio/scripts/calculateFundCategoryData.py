3254173a03cb9b8734956e896bd8b170afeb05c5
klq26-amd955 <klq26@163.com>
2019-10-26 14:14:36 +0800

    完成了 js 生成的部分功能。通过 python 组织对象并已经生成了正确的 json 字符串。但是浏览器 js 不支持 node.js 模块，暂时无法读取

--- Portfolio/scripts/calculateFundCategoryData.py
+++ Portfolio/scripts/calculateFundCategoryData.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+import os
+import json
 # for reduce method of lambda
 from functools import reduce
 # groupby & itemgetter
@@ -61,6 +62,31 @@ class calculateFundCategoryData:
             
             print(u'{0} 市值：{1}\t占比：{2}%\t盈亏：{3}'.format(category, self.beautify(marketCap), self.beautify(marketCap / totalMarketCap * 100), self.beautify(gain)))
 
+    def colorForCategory1(self,category1):
+        #self.category1Array = [u'A 股',u'海外新兴',u'海外成熟',u'债券',u'商品']
+        
+        # js code
+        #var aStockColor = {color: '#0aa3b5'};			// A 股（大盘股，中小盘股，红利价值，行业股）
+        #var outSideNewColor = {color: '#187a2f'};		// 海外新兴（香港，海外互联网）
+        #var outSideMatureColor = {color: '#ebb40f'};	// 海外成熟（德国）
+        #var universalGoodsColor = {color: '#dd4c51'};	// 商品（黄金，白银，原油）
+        #var bondColor = {color: '#be8663'};				// 债券（可转债，美元债）
+        #var cashColor = {color: '#f7a128'};				// 低风险理财（货币基金，地产项目）
+        #var frozenCashColor = {color: '#8b8c90'};		// 冻结资金（公积金）
+        
+        if category1 == self.category1Array[0]:
+            return '#0AA3B5'
+        elif category1 == self.category1Array[1]:
+            return '#187A2F'
+        elif category1 == self.category1Array[2]:
+            return '#EBB40F'
+        elif category1 == self.category1Array[3]:
+            return '#BE8663'
+        elif category1 == self.category1Array[4]:
+            return '#DD4C51'
+        else:
+            return 'FFFFFF'
+            
     def generateEchartsJson(self,modelArray):
         # 内部排序函数，按 category id 升序排列
         #def fundModelSortFunc(item):
@@ -77,36 +103,47 @@ class calculateFundCategoryData:
         totalMarketCap = self.beautify(totalMarketCap)
         #print(totalMarketCap)
         
-        # 一级分类
+        echarts = []
+        
+        # 树形结构化
         for category in self.category1Array:
-            echart1 = echartsModel()
             category1Models = [x for x in modelArray if x.category1 == category]
-            
+            # 一级分类
+            print(category)
+            # echarts 模型
+            echart1 = echartsModel()
+            echart1.name = category
+            echart1.value = 0.0
+            echart1.itemStyle['color'] = self.colorForCategory1(category)
             for category2, category2Array in groupby(category1Models,key=itemgetter('category2')):
-                print(category2)
-                for category3, category3Array in groupby(category1Models,key=itemgetter('category3')):
-                    
-                    for j in category3Array:
-                        if j.category2 == category2:
-                            print('\t' + category3)
-                    #print(len(list(category2Array)))
-                    for i in category3Array:
-                        if i.category2 == category2:
-                            print('\t\t{0}'.format(i.__dict__))
-            print('\n\n\n')
-            
-            
-            # 一级分类市值
-            #category1MarketCap = 0.0
-            #for cate1 in category1Models:
-            #    category1MarketCap = category1MarketCap + cate1.marketCap
-            #    echart1.name = cate1.category1
-            #    echart1.value = 0.0
-            #    echart1.itemStyle['color'] = '#0AA3B5'
-                
-                #for cate2 in category1Models:
-                    
-                #echart1.children.append(cate2)
-                
-            #print(category1MarketCap)
-            #print('\n')
+                # 二级分类
+                #print('-',category2)
+                # echarts 模型
+                echart2 = echartsModel()
+                echart2.name = category2
+                echart2.value = 0.0
+                echart2.itemStyle['color'] = echart1.itemStyle['color']
+                for category3, category3Array in groupby(category2Array,key=itemgetter('category3')):
+                    # 三级分类
+                    #print('--',category3)
+                    # echarts 模型
+                    echart3 = echartsModel()
+                    echart3.name = category3
+                    echart3.value = 0.0
+                    echart3.itemStyle['color'] = echart1.itemStyle['color']
+                    for model in category3Array:
+                        #print('----- {0}'.format(model.__dict__))
+                        echart3.value = echart3.value + round(float(model.marketCap),2)
+                    echart2.value = echart2.value + echart3.value   # 变成百分比之前，存入上级分类，下同
+                    echart3.value = round(float(echart3.value / totalMarketCap * 100),2)
+                    echart2.children.append(echart3.__dict__)
+                echart1.value = echart1.value + echart2.value   # 变成百分比之前，存入上级分类
+                print(echart1.value)
+                echart2.value = round(float(echart2.value / totalMarketCap * 100),2)
+                echart1.children.append(echart2.__dict__)
+            echart1.value = round(float(echart1.value / totalMarketCap * 100),2)
+            print(echart1.__dict__)
+            echarts.append(echart1.__dict__)
+        # 写入文件
+        with open(os.path.join(os.getcwd(),'config','echarts.json'),'w',encoding='utf-8') as jsonFile:
+            jsonFile.write(json.dumps(echarts))
---
+++ Portfolio/scripts/jsObjecFromJsonTester.js
@@ -0,0 +1,31 @@
+﻿var path = require('path'); //系统路径模块
+var fs = require('fs'); //文件模块
+
+var file = path.join(__dirname,'echarts.json'); //文件路径，__dirname为当前运行js文件的目录
+
+function getPortfolioData()
+{
+    console.log(file)
+    //读取json文件
+    fs.readFile(file, 'utf-8', function(err, data) {
+        if (err) {
+            console.log(err)
+            console.log('文件读取失败');
+        } else {
+            var obj = JSON.parse(data)
+            console.log(obj)
+            return obj
+        }
+    });
+}
+//var data = getPortfolioData()
+//jsonStr = JSON.stringify(data)
+//console.log(JSON.stringify(data))
+
+//var filePath = path.join(__dirname, 'config/echarts.json'); //文件路径，__dirname为当前运行js文件的目录
+
+//fs.writeFile(filePath, jsonStr, 'utf-8', function(err) {
+//    if (err) {
+//        throw err;
+//    }
+//});
