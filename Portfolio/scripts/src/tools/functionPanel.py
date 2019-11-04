3dc61b75eb8a7bf73da68fd63715cfc4186ab621
klq26-Ryzen2600 <klq26@163.com>
2019-11-04 09:23:48 +0800

    1. 增加 src 的 pathManager 路径。2. functionPanel 把功能按钮完成，增加了 6 个快捷开启功能（文件夹 & urls 等）。3. functionPanel 现在置顶，修改默认启动显示位置。

--- Portfolio/scripts/src/tools/functionPanel.py
+++ Portfolio/scripts/src/tools/functionPanel.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import subprocess
 
 # 子线程监听剪切板
 import threading
@@ -17,6 +18,7 @@ srcDir = os.path.dirname(currentDir)
 sys.path.append(srcDir)
 from config.pathManager import pathManager
 
+# 跨线程沟通 key
 thread_key_window = 'window'
 thread_key_clipboard_text_callback = 'callback'
 
@@ -45,17 +47,18 @@ class clipboardObserveThread(threading.Thread):
                 mainThreadCallback(txt)
             # 检测间隔（延迟0.2秒）
             time.sleep(0.5)
-        
 
 class functionPanel:
     def __init__(self):
         # 主窗口
         self.window = tk.Tk()
+        self.window.wm_attributes('-topmost',1)
+
         # 窗口标题
-        self.window.title('Https Header Saver')
+        self.window.title('Finance Data Operation Panel')
         # 窗口尺寸
-        self.window.geometry('640x480+120+800') # +{0} = origin.x 80 +{1} = origin.y
-        self.clipboardValue = StringVar()
+        self.window.geometry('542x360+100-100') # +{0} = origin.x 80 +{1} = origin.y
+        self.clipboardValue = StringVar()   
         self.clipboardValue.set(self.window.clipboard_get())
         self.createWidgets()
         # 开启剪切板监控子线程
@@ -64,15 +67,18 @@ class functionPanel:
         thread_key_clipboard_text_callback: self.clipboardTextUpdate   \
         })
         t.start()
-        
+    
+    # 创建子视图
     def createWidgets(self):
-        klqHeaderRows = [u'tiantian_klq.txt',u'guangfa_klq.txt',u'qieman_klq.txt',u'danjuan_klq.txt',u'xueqiu_klq.txt']
-        parentsHeadersRows = [u'tiantian_lsy.txt',u'tiantian_ksh.txt', u'danjuan_lsy.txt',u'danjuan_ksh.txt']
-        operations = [u'抓取所有基金持仓',u'资产配置组合',u'持仓市值估计',u'一键开启相关网站']
+        klqHeaders = [u'tiantian_klq.txt',u'guangfa_klq.txt',u'qieman_klq.txt',u'danjuan_klq.txt',u'xueqiu_klq.txt']
+        parentsHeaders = [u'tiantian_lsy.txt',u'tiantian_ksh.txt', u'danjuan_lsy.txt',u'danjuan_ksh.txt']
+        
+        shorthands = [u'all webs', u'src Folder', u'config Folder',u'input Folder', u'output Folder',u'echarts Folder']
+        operations = [u'allFundSpider a',u'allFundSpider b',u'assetCombine c',u'assetCombine b', u'estimateExcel a', u'estimateExcel b']
         col = 0
-        tk.Label(self.window,text=u'康力泉 Headers',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
-        for i in range(1,len(klqHeaderRows)+1):
-            row = klqHeaderRows[i-1]
+        tk.Label(self.window,text=u'My Headers',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
+        for i in range(1,len(klqHeaders)+1):
+            row = klqHeaders[i-1]
             # tkinter 要求由按钮（或者其它的插件）触发的控制器函数不能含有参数，如果有，需要 lambda，下同
             button = tk.Button(self.window, text=row,width=15,height=1, bg="#F7A128")
             button.grid(row=i, column=col, padx=10, pady=10)
@@ -81,24 +87,34 @@ class functionPanel:
             # 为左键双击事件绑定处理方法
             #bn.bind('<Double-1>', self.double)
         col = 1
-        tk.Label(self.window,text=u'父母 Headers',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
-        for i in range(1, len(parentsHeadersRows)+1):
-            row = parentsHeadersRows[i-1]
+        tk.Label(self.window,text=u'Parents Headers',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
+        for i in range(1, len(parentsHeaders)+1):
+            row = parentsHeaders[i-1]
             button = tk.Button(self.window, text=row,width=15,height=1, bg="#F2C300")
             button.grid(row=i, column=col, padx=10, pady=10)
             # 为左键单击事件绑定处理方法
             button.bind(u'<Button>', self.saveHeaderToFile)
+            
         col = 2
-        tk.Label(self.window,text=u'操作',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
+        tk.Label(self.window,text=u'shorthands',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
+        for i in range(1, len(shorthands)+1):
+            row = shorthands[i-1]
+            button = tk.Button(self.window, text=row,width=15,height=1, bg="#BE8663")
+            button.grid(row=i, column=col, padx=10, pady=10)
+            # 为左键单击事件绑定处理方法
+            button.bind(u'<Button>', self.shorthandSelect)
+
+        col = 3
+        tk.Label(self.window,text=u'operations',width=15,height=1).grid(row=0, column=col, padx=10, pady=10)
         for i in range(1, len(operations)+1):
             row = operations[i-1]
             button = tk.Button(self.window, text=row,width=15,height=1, bg="#00B1CC")
             button.grid(row=i, column=col, padx=10, pady=10)
             # 为左键单击事件绑定处理方法
             button.bind(u'<Button>', self.functionSelect)
-            
-        tk.Label(self.window,textvariable=self.clipboardValue,width=55,height=8,bg="#00B1CC").grid(row=6, column=0,columnspan=3, padx=10, pady=10)
-    
+        #tk.Label(self.window,textvariable=self.clipboardValue,width=55,height=8,bg="#00B1CC").grid(row=6, column=0,columnspan=3, padx=10, pady=10)
+
+    # 保存 header 到文件
     def saveHeaderToFile(self, event):
         #print("左键单击:%s" % event.widget['text'])
         clipboardText = self.clipboardValue.get()
@@ -114,9 +130,49 @@ class functionPanel:
                 f.write(clipboardText)
             result = tkinter.messagebox.showinfo('', u'保存成功')
     
+    # 快捷打开多个文件夹
+    def shorthandSelect(self,event):
+        # print("打开：%s" % event.widget['text'].replace(' Folder',''))
+        param = event.widget['text'].replace(' Folder','')
+        pm = pathManager()
+        if param == u'src':
+            os.startfile(pm.parentDir)
+        if param == u'config':
+            os.startfile(pm.configPath)
+        if param == u'input':
+            os.startfile(pm.inputPath)
+        if param == u'output':
+            os.startfile(pm.outputPath)
+        if param == u'echarts':
+            os.startfile(pm.echartsPath)
+        if param == u'all webs':
+            # 打开所有网页
+            os.startfile(u'http://www.1234567.com.cn/')
+            os.startfile(u'http://www.gffunds.com.cn/')
+            os.startfile(u'http://www.qieman.com/')
+            os.startfile(u'http://www.danjuanapp.com/')
+            os.startfile(u'http://www.xueqiu.com/')
+        
     def functionSelect(self,event):
         print("功能选择：%s" % event.widget['text'])
-    
+        params = event.widget['text'].split(' ')
+        pm = pathManager()
+        if params[0] == u'allFundSpider':
+            pyFile = os.path.join(pm.parentDir,u'allFundSpider.py')
+            args = [r"powershell","python",pyFile,params[1]]
+            print('[Executing] {0} ...'.format(pyFile))
+            p = subprocess.Popen(args)
+        if params[0] == u'assetCombine':
+            pyFile = os.path.join(pm.parentDir,u'assetAllocationCombine.py')
+            args = [r"powershell","python",pyFile,params[1]]
+            print('[Executing] {0} ...'.format(pyFile))
+            p = subprocess.Popen(args)
+        if params[0] == u'estimateExcel':
+            pyFile = os.path.join(pm.parentDir,u'assetAllocationEstimateExcelParser.py')
+            args = [r"powershell","python",pyFile,params[1]]
+            print('[Executing] {0} ...'.format(pyFile))
+            p = subprocess.Popen(args)
+        
     def isSupposeToBeHeaderText(self,text):
         if text == '' or text == None:
             return False
@@ -177,7 +233,7 @@ class functionPanel:
             else:
                 print(result)
             return
-        # 尝试匹配天天基金 - 康力泉
+        # 尝试匹配天天基金 - 康力泉    POST /do.aspx/CheckLogin HTTP/1.1
         #if u'gr_user_id=07a8b8b8-e04f-4bfa-b6bd-3492bb42b3a6' in text:
         #    print(u'[Success] 成功匹配到网站 Header：蛋卷基金 - 康力泉')
         #    result = tkinter.messagebox.askokcancel('成功匹配“蛋卷基金 - 康力泉”', '要把剪切板上的内容覆盖到 danjuan_klq.txt 吗？')
