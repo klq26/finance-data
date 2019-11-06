a1409e736546fa4bce86236eb08d237f8fb1f344
klq26-amd955 <klq26@163.com>
2019-11-06 18:27:57 +0800

    更新持仓市值，更新 echarts 文案颜色为 #333333，修改 allFundSpiderMultiProcess 无法正确打开各个 Spider 的问题（路径引用不全）

--- Portfolio/scripts/src/allFundSpiderMultiProcess.py
+++ Portfolio/scripts/src/allFundSpiderMultiProcess.py
@@ -16,27 +16,28 @@ class allFundSpiderMultiProcess:
         self.parentSpidersArray = ['tiantianSpider.py','danjuanSpider.py']
 
     def getKLQ(self):
+        fileNameExt = u'康力泉'
+        self.pm = pathManager(strategyName=fileNameExt)
         # 多线程并发（都传策略 a）
         for pyFile in self.klqSpiderArray:
-            args = [r"powershell","python",pyFile,"a"]
+            args = [r"powershell","python",os.path.join(self.pm.parentDir, pyFile),"a"]
             print('[Executing] {0}...'.format(pyFile))
             p = subprocess.Popen(args)
         # 拷贝文件
-        fileNameExt = u'康力泉'
-        self.pm = pathManager(strategyName=fileNameExt)
         fileName = u'cash_{0}.txt'.format(fileNameExt)
         shutil.copy(os.path.join(self.pm.inputPath,fileName),os.path.join(self.pm.holdingOutputPath,fileName))
         fileName = u'freeze_{0}.txt'.format(fileNameExt)
         shutil.copy(os.path.join(self.pm.inputPath,fileName),os.path.join(self.pm.holdingOutputPath,fileName))
     
     def getParent(self):
-        # 多线程并发（都传策略 b）
-        for pyFile in self.parentSpidersArray:
-            args = [r"powershell","python",pyFile,"b"]
-            print('[Executing] {0}...'.format(pyFile))
-            p = subprocess.Popen(args)
         fileNameExt = u'父母'
         self.pm = pathManager(strategyName=fileNameExt)
+        # 多线程并发（都传策略 b）
+        for pyFile in self.parentSpidersArray:
+            args = [r"powershell","python",os.path.join(self.pm.parentDir, pyFile),"b"]
+            print('[Executing] {0}...'.format(pyFile))
+            p = subprocess.Popen(args)
+        
 
 if __name__ == '__main__':
     strategy = 'a'
