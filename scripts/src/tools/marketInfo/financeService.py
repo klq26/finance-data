0f61d025dd34c7e482bb3420096cb719c779b010
klq26-amd955 <klq26@163.com>
2020-03-02 11:43:48 +0800

    1. 修复了一个早上9点拿沪深净流入数据时，接口崩溃的问题。 2. 为每个模块增加了开盘收盘时间显示。（以后可以做折叠后默认不更新，展开再更新） 3. 调整了a股下发指数的顺序，等权800夹在500和1000之间还是感觉别扭。

--- scripts/src/tools/marketInfo/financeService.py
+++ scripts/src/tools/marketInfo/financeService.py
@@ -66,7 +66,7 @@ def getIndexInfos():
 
 # 请求中国
 def requestChinaIndexs():
-    url = "http://10.push2.eastmoney.com/api/qt/clist/get?cb=updateIndexInfos&pn=1&pz=30&fs=i:1.000832,i:1.000001,i:0.399001,i:0.399006,i:1.000015,i:1.000922,i:1.000016,i:1.000300,i:0.399905,i:1.000842,i:1.000852,i:0.399005,i:1.000991,i:1.000992,i:0.399975,i:0.399986,i:0.399812,i:0.399971,i:1.000827,i:100.HSI,i:100.HSCEI,i:124.HSCCI&fields=f14,f12,f2,f4,f3,f18,f6"
+    url = "http://10.push2.eastmoney.com/api/qt/clist/get?cb=updateIndexInfos&pn=1&pz=30&fs=i:1.000832,i:1.000001,i:0.399001,i:0.399006,i:1.000015,i:1.000922,i:1.000016,i:1.000300,i:0.399905,i:1.000852,i:1.000842,i:0.399005,i:1.000991,i:1.000992,i:0.399975,i:0.399986,i:0.399812,i:0.399971,i:1.000827,i:100.HSI,i:100.HSCEI,i:124.HSCCI&fields=f14,f12,f2,f4,f3,f18,f6"
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     response = requests.get(url, headers=headers, verify=False)
     if response.status_code == 200:
@@ -410,7 +410,10 @@ def getMoneyInfo():
         data = response.text.replace('updateMoneyFlow(','').replace(');','')
         jsonData = json.loads(data)
         klines = jsonData["data"]["klines"]
-        lastestData = klines[-1].split(',')
+        if len(klines) > 0:
+            lastestData = klines[-1].split(',')
+        else:
+            lastestData = ["0","0","0","0","0","0"]
         # print(lastestData)
         # 取值
         # ["2020-02-21 15:00", "-25845607550.0", "27224361803.0", "-1378753893.0", "-18919648215.0", "-6925959335.0"]
